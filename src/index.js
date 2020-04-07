500c2965fc4b603d8cb4dbab27bf39dd8a57ae0f
Alex Kayser <akctba@gmail.com>
2020-04-06 17:28:16 -0700

    fetch and first styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import AppBoot from './AppBoot';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <AppBoot /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
